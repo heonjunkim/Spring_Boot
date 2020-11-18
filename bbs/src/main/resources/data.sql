9edb5c7fa66d560e012daa995d082492336ae5af
heonjunkim <maplegjswns@naver.com>
2020-11-18 16:46:08 +0900

    1

---
+++ bbs/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO tbl_bbs (b_writer, b_date_time, b_content,b_count)
+VALUES('홍길동','2020-11-18','게시판 만들기',0);
+INSERT INTO tbl_bbs (b_writer, b_date_time, b_content,b_count)
+VALUES('이모룡','2020-11-18','게시판 만들기',0);
+INSERT INTO tbl_bbs (b_writer, b_date_time, b_content,b_count)
+VALUES('성춘향','2020-11-18','게시판 만들기',0);
